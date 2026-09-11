ede109dec82216d5cf14c1c9e204814c56ee3434
cristianvega1995 <79797831+cristianvega1995@users.noreply.github.com>
2026-09-11 00:59:24 -0300

    Add CSS rules for toggle list visibility

--- script1.js
+++ script1.js
@@ -15,3 +15,21 @@ function updateNews() {
 }
 
 updateNews();
+/*
+Source - https://stackoverflow.com/a/24978113
+Posted by Roko C. Buljan, modified by community. See post 'Timeline' for change history
+Retrieved 2026-09-11, License - CC BY-SA 4.0
+*/
+
+[id^="togList"],                        /* HIDE CHECKBOX */
+[id^="togList"] ~ .list,                /* HIDE LIST */
+[id^="togList"] + label  span + span,   /* HIDE "Collapse" */
+[id^="togList"]:checked + label span{   /* HIDE "Expand" (IF CHECKED) */
+  display:none;
+}
+[id^="togList"]:checked + label span + span{
+  display:inline-block;                 /* SHOW "Collapse" (IF CHECKED) */
+}
+[id^="togList"]:checked ~ .list{
+  display:block;                        /* SHOW LIST (IF CHECKED) */
+}
